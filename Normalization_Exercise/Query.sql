f568dd09d578762ab045a60f00faa489f25eb7fa
Stan Chen <43565411+stan587@users.noreply.github.com>
2019-10-28 14:16:21 -0600

    Update Query.sql

--- Normalization_Exercise/Query.sql
+++ Normalization_Exercise/Query.sql
@@ -1,5 +1,40 @@
 -- Dataset extracted from https://www.kaggle.com/pavansubhasht/ibm-hr-analytics-attrition-dataset/download
 use normalization_practice
 -- ALTER TABLE HREmployeeAttrition ADD employeeID int IDENTITY(10001,1);
+-- SELECT * FROM HREmployeeAttrition;
 
-SELECT * FROM HREmployeeAttrition;
+DROP Table NormalizeTable;
+DROP Table normalizeBusiness;
+DROP Table normalizeDepartment;
+DROP Table normalizeEducationField;
+
+SELECT employeeID,BusinessTravel,Department,EducationField 
+INTO NormalizeTable
+FROM HREmployeeAttrition;
+
+SELECT * FROM NormalizeTable;
+
+
+SELECT DISTINCT BusinessTravel
+INTO normalizeBusiness
+FROM NormalizeTable;
+ALTER TABLE NormalizeBusiness ADD businessID int IDENTITY(1,1);
+SELECT * FROM normalizeBusiness;
+
+
+
+SELECT DISTINCT Department
+INTO normalizeDepartment
+FROM NormalizeTable;
+ALTER TABLE normalizeDepartment ADD departmentID int IDENTITY(1,1);
+SELECT * FROM normalizeDepartment;
+
+
+SELECT DISTINCT EducationField
+INTO normalizeEducationField
+FROM NormalizeTable;
+ALTER TABLE normalizeEducationField ADD educationFieldID int IDENTITY(1,1);
+SELECT * FROM normalizeEducationField;
+
+
+
