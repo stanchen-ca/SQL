b4a44ca4ae5903937cc7d645538c5efdf2698680
Stan Chen <43565411+stan587@users.noreply.github.com>
2019-10-28 13:55:21 -0600

    Create Query.sql

---
+++ Normalization_Exercise/Query.sql
@@ -0,0 +1,5 @@
+-- Dataset extracted from https://www.kaggle.com/pavansubhasht/ibm-hr-analytics-attrition-dataset/download
+use normalization_practice
+-- ALTER TABLE HREmployeeAttrition ADD employeeID int IDENTITY(10001,1);
+
+SELECT * FROM HREmployeeAttrition;
